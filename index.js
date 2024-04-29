afcaa208378fc45d9a57ffd790746331b6bc5cef
Ayush verma <120269805+Ayushverma135@users.noreply.github.com>
2024-04-30 01:29:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".animated-progress span").each(function () {
+    $(this).animate(
+      {
+        width: $(this).attr("data-progress") + "%",
+      },
+      1000
+    );
+    $(this).text($(this).attr("data-progress") + "%");
+  });
